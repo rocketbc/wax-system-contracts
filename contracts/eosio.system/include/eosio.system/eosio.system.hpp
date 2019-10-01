1de3b4d6cb744dabcdaf2e156cfe274fefeb7f79
Jae Chung <jae.chung99@gmail.com>
2019-10-01 13:57:00 -0400

    quick fix on serialization

--- contracts/eosio.system/include/eosio.system/eosio.system.hpp
+++ contracts/eosio.system/include/eosio.system/eosio.system.hpp
@@ -284,7 +284,7 @@ namespace eosiosystem {
       };
 
       // explicit serialization macro is not necessary, used here only to improve compilation time
-      EOSLIB_SERIALIZE( voter_info, (owner)(proxy)(producers)(proposals)(staked)(unpaid_voteshare)(unpaid_voteshare_last_updated)(unpaid_voteshare_change_rate)
+      EOSLIB_SERIALIZE( voter_info, (owner)(proxy)(producers)(staked)(unpaid_voteshare)(unpaid_voteshare_last_updated)(unpaid_voteshare_change_rate)
                                     (last_claim_time)(last_vote_weight)(proxied_vote_weight)(is_proxy)(flags1)(reserved2)(reserved3) )
    };
 
